92d573c5ba91f6009b088382c8661334f9b6b9b3
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 22:25:20 -0300

    Commit Inicial

---
+++ src/test/java/com/bn/ex9/Ex9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.ex9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
